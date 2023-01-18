51d651e3fc604116bb23a3ef74dc87fa85077575
Daniel Mazali <danieldelimamazali@hotmail.com>
2023-01-17 22:12:54 -0300

    teste

--- www/conn.php
+++ www/conn.php
@@ -3,6 +3,5 @@ try {
     $pdo = new PDO("mysql:host=bd-php-app.mysql.database.azure.com;dbname=banco", "mazali", "ProxyONE001");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("PHP é uma bosta: o banco deu pau, vai lá resolver isso seu merda" . $e->getMessage());
+    die("ERRO:\n" . $e->getMessage());
 }
-// bosta de php, odeio php, não gosto de php
