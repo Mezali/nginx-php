4358cf131d2340d5caaa61aabdc1154db9f66245
Daniel Mazali <danieldelimamazali@hotmail.com>
2023-01-19 14:29:24 -0300

    teste

--- www/index.php
+++ www/index.php
@@ -10,8 +10,6 @@
   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
   <link rel="manifest" href="manifest.json">
   <script src="js/script.js"></script>
-
-
 </head>
 
 <body>
