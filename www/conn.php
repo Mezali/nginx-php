55a0b5b1336d4ed28acb1b4bc2214717a3368d03
Daniel Mazali <danieldelimamazali@hotmail.com>
2023-01-26 00:01:29 -0300

    ip

--- www/conn.php
+++ www/conn.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=74.207.232.83;dbname=banco", "user", "ProxyONE001");
+    $pdo = new PDO("mysql:host=139.144.62.153;dbname=banco", "user", "ProxyONE001");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conectado";
 } catch (PDOException $e) {
